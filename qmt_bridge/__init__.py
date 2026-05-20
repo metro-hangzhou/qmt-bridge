aad45b461257c0078dc0f14d83919ef2fa719c75
2233admin <1497479966@qq.com>
2026-05-21 01:39:14 +0800

    feat: add QMTBridge unified router (Refs: XAR-498)

--- qmt_bridge/__init__.py
+++ qmt_bridge/__init__.py
@@ -1,3 +1,8 @@
 """qmt-bridge: QMT broker bridge with 大QMT HTTP + miniQMT xtquant dual-mode support."""
 
+from .bridge import QMTBridge
+from .daqmt_bridge import DaQMTBridge
+from .xtquant_bridge import XtQuantBridge
+
 __version__ = "0.1.0"
+__all__ = ["QMTBridge", "DaQMTBridge", "XtQuantBridge"]
